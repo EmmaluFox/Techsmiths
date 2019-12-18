0ca0c6bc0f7250f60fa217b16d0b943b45582bab
Emmalu Fox <47330108+EmmaluFox@users.noreply.github.com>
2019-12-18 17:27:49 +0000

    Operator Choice Boolean

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const readline = require('readline-sync');
-console.log('Hello! Lets do some multiplication!');
-console.log('Pick a number, any number...');
-const result = readline.prompt();
+console.log('Hello! Lets do some mathematics!');
+console.log('Pick an operator: +, -. / or *');
+const sign = readline.keyIn();
+console.log('Pick a number, any number');
+const result = readline.questionInt();
 console.log('Pick another number, I love numbers!');
-const response2 = readline.prompt();
-const answer = ((result)*(response2));
-console.log(result +' multiplied by ' +response2 +' equals ' +answer +'. Clever me!');
+const response2 = readline.questionInt();
+if (sign == '+') {
+    var answer = ((result) + (response2));
+  } else if (sign == '*') {
+    var answer = ((result) * (response2));
+  } else if (sign == '-') {
+    var answer = ((result) - (response2));
+  } else if (sign == '/') {
+    var answer = ((result) / (response2));
+  } else {
+      console.log('Error!')
+  }
+console.log(result +sign +response2 +' equals ' +answer +'. Clever me!');
