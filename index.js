12d3e6b985734803688ed26dc477fd8d7132a0d4
Emmalu Fox <47330108+EmmaluFox@users.noreply.github.com>
2019-12-18 13:52:10 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const readline = require('readline-sync');
-
 console.log('Hello! Lets do some multiplication!');
 console.log('Pick a number, any number...');
-		const result = readline.prompt();
-
+const result = readline.prompt();
 console.log('Pick another number, I love numbers!');
-		const response2 = readline.prompt();
-		const answer = ((result)*(response2));
-
+const response2 = readline.prompt();
+const answer = ((result)*(response2));
 console.log(result +' multiplied by ' +response2 +' equals ' +answer +'. Clever me!');
