80a290adcbdb348de7d8dcf793f32516cca71135
Emmalu Fox <47330108+EmmaluFox@users.noreply.github.com>
2019-12-18 13:50:36 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const readline = require('readline-sync');
+
+console.log('Hello! Lets do some multiplication!');
+console.log('Pick a number, any number...');
+		const result = readline.prompt();
+
+console.log('Pick another number, I love numbers!');
+		const response2 = readline.prompt();
+		const answer = ((result)*(response2));
+
+console.log(result +' multiplied by ' +response2 +' equals ' +answer +'. Clever me!');
